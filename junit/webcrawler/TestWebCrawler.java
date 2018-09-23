9764897b5f4062767fa5eb7ad02a3249259a0eec
David Bohlin <dbbohlin@gmail.com>
2018-09-23 12:11:34 -0700

    Get Base Site and Test for Exceptions

--- junit/webcrawler/TestWebCrawler.java
+++ junit/webcrawler/TestWebCrawler.java
@@ -2,8 +2,13 @@ package webcrawler;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class TestWebCrawler {
 
@@ -16,13 +21,35 @@ class TestWebCrawler {
 
 	@Test
 	void testWebCrawlerNullorEmptySiteReturnsNull() {
-		assertEquals(null, crawler.crawl(null));
+		try {
+			assertEquals(null, crawler.crawl(null));
+		} catch (URISyntaxException | MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	void testWebCrawlerOpenWebSite() {
-		String crawlMap = crawler.crawl("http://wiprodigital.com");
-		
+		String crawlMap = null;
+		try {
+			crawlMap = crawler.crawl("http://wiprodigital.com");
+		} catch (URISyntaxException | MalformedURLException e) {
+			e.printStackTrace();
+		}
+		System.out.println(crawlMap);
 	}
-
+	
+	@Test
+	void testWebCrawlerBadSiteThrowsURISyntaxException() {
+		Assertions.assertThrows(URISyntaxException.class, () ->{
+					crawler.crawl("http://www. thest.com");
+				});
+	}
+	
+	@Test
+	void testWebCrawlerBadSiteThrowsMalformedURLException() {
+		Assertions.assertThrows(MalformedURLException.class, () ->{
+					crawler.crawl("thest.com");
+				});
+	}
 }
--- src/webcrawler/WebCrawler.java
+++ src/webcrawler/WebCrawler.java
@@ -2,6 +2,12 @@
  * 
  */
 package webcrawler;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashSet;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,14 +18,30 @@ import org.jsoup.select.Elements;
  *
  */
 public class WebCrawler {
-
-	public String crawl(String site) {
-		if(null == site || site.isEmpty()) {
+	private HashSet<String> links;
+	private String baseSite = "";
+	
+	public void webCrawler() {
+		this.links = new HashSet<String>();
+	}
+	
+	public String crawl(String site) throws URISyntaxException, MalformedURLException {
+		if(isSiteNullorEmpty(site)) {
 			return null;
 		}
+		setBaseSite(site);
 		StringBuilder builder = new StringBuilder();
 		
 		return builder.toString();
+	}
+		
+	private void setBaseSite(String site) throws URISyntaxException, MalformedURLException {
+		URI uri = new URL(site).toURI();
+		this.baseSite = uri.getHost();
+	}
+	
+	private boolean isSiteNullorEmpty(String site) {
+		return null == site || site.isEmpty();
 	}
 
 }
